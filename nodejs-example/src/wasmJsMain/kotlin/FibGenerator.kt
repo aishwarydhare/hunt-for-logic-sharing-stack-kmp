93fffb9d1c48a9260fffe06beaa4817fa17cdfb3
Aishwary Dhare <aishwary.dhare@gmail.com>
2024-05-12 09:50:46 +0400

    fib-gen and yarn lock upgrade

---
+++ nodejs-example/src/wasmJsMain/kotlin/FibGenerator.kt
@@ -0,0 +1,12 @@
+data class FibonacciInput(val count: Int)
+
+class FibonacciGenerator {
+    fun generate(input: FibonacciInput): List<Int> {
+        if (input.count < 1) return emptyList()
+        val sequence = mutableListOf(0, 1)
+        while (sequence.size < input.count) {
+            sequence.add(sequence[sequence.size - 1] + sequence[sequence.size - 2])
+        }
+        return sequence.take(input.count)
+    }
+}
